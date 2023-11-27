978635f811b043da36ecabc8d1dd9af21a805461
sakkkko0718 <sa-tofu882@ezweb.ne.jp>
2023-11-27 14:43:40 +0900

    画像の登録

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -3,12 +3,14 @@
 
 @section('content')
 <table>
-    <tr class="table-head"><th>部屋の名前</th><th>部屋の種類</th><th>部屋の写真</th></tr>
+    <tr class="table-head"><th>部屋の名前</th><th>部屋の種類</th><th>写真</th></tr>
         @foreach ($rooms as $room)
         <tr>
             <td>{{$room->room_num}}</td>
             <td>{{$room->master->room_name}}</td>
-            <td>いい部屋の写真？</td>
+            {{-- <td><img src="{{$room->image}}" alt=""></td> --}}
+            {{-- publicに画像を保存、DBには画像の名前を登録 --}}
+            <td><img src="{{asset('image/'.$room->image.'.png')}}" alt=""></td>
         </tr>
         @endforeach
 </table>
