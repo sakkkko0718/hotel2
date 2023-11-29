fb70ab1c8eb7394ec2649bacebdbf996f72d148d
sakkkko0718 <sa-tofu882@ezweb.ne.jp>
2023-11-29 15:20:10 +0900

    コメントの削除

--- app/Master.php
+++ app/Master.php
@@ -8,10 +8,6 @@ class Master extends Model
 {
     protected $primaryKey = 'master_id';
 
-    // public static function getData($input){
-    //     $data = Master::whereBetween('')
-    // }
-
     public function rooms(){
         return $this->hasMany('App\Room','master_id','master_id');
     }
